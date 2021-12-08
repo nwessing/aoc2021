e6042a9e0c0792632e11765725bb2b3b2fdf655f
Nick Wessing <nwessing3@gmail.com>
2021-12-07 18:22:52 -0600

    Day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,8 +1,9 @@
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Point {
     x: i32,
     y: i32,
@@ -12,6 +13,12 @@ struct Point {
 struct LineSegment {
     start: Point,
     end: Point,
+}
+
+#[derive(Clone)]
+struct IntersectionTracker {
+    part1: i32,
+    part2: i32,
 }
 
 fn parse_point(mut input: std::str::Split<&str>) -> Result<Point, &'static str> {
@@ -34,10 +41,6 @@ pub fn solve(file_input: File) -> Result<(i64, i64), &'static str> {
     let mut input_line = String::new();
 
     let mut lines: Vec<LineSegment> = Vec::with_capacity(1000);
-    // let mut grid_min = Point {
-    //     x: i32::MAX,
-    //     y: i32::MAX,
-    // };
     let mut grid_max = Point { x: 0, y: 0 };
     while reader
         .read_line(&mut input_line)
@@ -50,8 +53,6 @@ pub fn solve(file_input: File) -> Result<(i64, i64), &'static str> {
 
         let start = parse_point(start)?;
         let end = parse_point(end)?;
-        // grid_min.x = grid_min.x.min(start.x).min(end.x);
-        // grid_min.y = grid_min.y.min(start.y).min(end.y);
         grid_max.x = grid_max.x.max(start.x).max(end.x);
         grid_max.y = grid_max.y.max(start.y).max(end.y);
 
@@ -59,45 +60,53 @@ pub fn solve(file_input: File) -> Result<(i64, i64), &'static str> {
 
         input_line.clear();
     }
-    // println!("max = {:?}, min = {:?}", grid_max, grid_min);
 
-    // let stride = grid_max.x - grid_min.x;
     let stride = grid_max.x + 1;
-    // let mut grid: Vec<i32> = vec![0; ((max.x - min.x + 1) * (max.y - min.y + 1)) as usize];
-    let mut grid: Vec<i32> = vec![0; ((grid_max.x + 1) * (grid_max.y + 1)) as usize];
+    let mut grid: Vec<IntersectionTracker> = vec![
+        IntersectionTracker { part1: 0, part2: 0 };
+        ((grid_max.x + 1) * (grid_max.y + 1)) as usize
+    ];
     for line in lines {
-        if line.start.x != line.end.x && line.start.y != line.end.y {
-            continue;
-        }
+        let dx = match line.start.x.cmp(&line.end.x) {
+            Ordering::Greater => -1,
+            Ordering::Less => 1,
+            Ordering::Equal => 0,
+        };
+        let dy = match line.start.y.cmp(&line.end.y) {
+            Ordering::Greater => -1,
+            Ordering::Less => 1,
+            Ordering::Equal => 0,
+        };
 
-        let x_start = line.start.x.min(line.end.x);
-        let x_end = line.start.x.max(line.end.x);
-        if x_start != x_end {
-            for x in x_start..=x_end {
-                grid[(x + (line.start.y * stride)) as usize] += 1;
+        let mut point = line.start.clone();
+        loop {
+            let tracker = &mut grid[(point.x + (point.y * stride)) as usize];
+            tracker.part2 += 1;
+            if !(dx != 0 && dy != 0) {
+                tracker.part1 += 1;
             }
-        }
 
-        let y_start = line.start.y.min(line.end.y);
-        let y_end = line.start.y.max(line.end.y);
-        if y_start != y_end {
-            for y in y_start..=y_end {
-                grid[(line.start.x + (y * stride)) as usize] += 1;
+            if point == line.end {
+                break;
+            }
+            point.x += dx;
+            point.y += dy;
+
+            if point.x > grid_max.x || point.y > grid_max.y || point.x < 0 || point.y < 0 {
+                break;
             }
         }
     }
 
-    let part1 = grid.iter().filter(|x| **x > 1).count() as i64;
-
-    // for line in lines {
-    //     println!(
-    //         "{},{} -> {},{}",
-    //         line.start.x, line.start.y, line.end.x, line.end.y
-    //     );
-    // }
-
-    // let part1 = 0i64;
-    let part2 = 0i64;
-
+    let mut part1 = 0i64;
+    let mut part2 = 0i64;
+    for tracker in grid.iter_mut() {
+        if tracker.part1 > 1 {
+            part1 += 1;
+        }
+        if tracker.part2 > 1 {
+            part2 += 1;
+        }
+    }
     Ok((part1, part2))
 }
